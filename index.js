b792a19e0831f4c515077c01427be810afa77a32
Mickel <mickelkelly04@gmail.com>
2025-02-19 15:54:49 -0400

    lab 5 Exercise started

--- index.js
+++ index.js
@@ -1,5 +1,66 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [ 89, 34, 67 ]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [ 100, 89, 79 ]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [ 89, 34, 67 ]
+    }
+  ]
+};
+
+
+const students = [bob, sally, paul]; 
+
+function getAverateGrade(student, course) {
+ 
+  for ( let trans of student.transcript) {
+    let sum = 0;
+    for (let grades of trans.grades) {
+      sum += grades; 
+    }
+      return sum / trans.grades.length;
+  }
+} 
+
+console.log(getAverateGrade(bob, 'INFO 1601')); 
+
+function getAssignmentMark(student, course, num) {
+
+
 }
+
+console.log(getAssignmentMark(bob, 'INFO 1601')); 
